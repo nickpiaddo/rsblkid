8581725ce348eadeabe1c4dcbdefa372b144b1d4
Nick Piaddo <nick.piaddo@protonmail.com>
2024-08-24 08:38:21 +0000

    Create enum `parser_error`

--- src/error.rs
+++ src/error.rs
@@ -9,6 +9,7 @@ use thiserror::Error;
 // From standard library
 
 // From this library
+use crate::core::errors::ParserError;
 
 /// A specialized [`Result`](std::result::Result) type for `rsblkid`.
 ///
@@ -33,4 +34,7 @@ pub type Result<T> = std::result::Result<T, RsBlkidError>;
 /// ```
 #[derive(Debug, Error)]
 #[non_exhaustive]
-pub enum RsBlkidError {}
+pub enum RsBlkidError {
+    #[error(transparent)]
+    Parser(#[from] ParserError),
+}
