3c644f8324e4b5bc3ce6a8f93464d0eca2b5ad16
Nick Piaddo <nick.piaddo@protonmail.com>
2024-08-24 08:33:13 +0000

    Create module `core::device`

--- src/core/device/mod.rs
+++ src/core/device/mod.rs
@@ -1,15 +1,10 @@
 // Copyright (c) 2023 Nick Piaddo
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Shared objects and helper functions.
+//! Device objects and helper functions.
 
 // From dependency library
 
 // From standard library
 
 // From this library
-pub mod device;
-pub mod errors;
-pub mod num;
-pub mod partition;
-pub mod utils;
