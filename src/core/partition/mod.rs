96d7b40490e6998d999d7424f2ddcaec9965e86f
Nick Piaddo <nick.piaddo@protonmail.com>
2024-08-24 12:05:45 +0000

    Create module `core::partition`

--- src/core/partition/mod.rs
+++ src/core/partition/mod.rs
@@ -1,13 +1,10 @@
 // Copyright (c) 2023 Nick Piaddo
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Shared objects and helper functions.
+//! Module for handling disk partitions.
 
 // From dependency library
 
 // From standard library
 
 // From this library
-pub mod errors;
-pub mod partition;
-pub mod utils;
