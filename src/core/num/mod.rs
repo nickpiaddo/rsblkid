75b9f38ef3fdc2137fe3a06d93f495903cca99a8
Nick Piaddo <nick.piaddo@protonmail.com>
2024-08-26 07:10:50 +0000

    Create module `core::num`

--- src/core/num/mod.rs
+++ src/core/num/mod.rs
@@ -1,14 +1,10 @@
 // Copyright (c) 2023 Nick Piaddo
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Shared objects and helper functions.
+//! Arithmetic objects and helper functions.
 
 // From dependency library
 
 // From standard library
 
 // From this library
-pub mod errors;
-pub mod num;
-pub mod partition;
-pub mod utils;
