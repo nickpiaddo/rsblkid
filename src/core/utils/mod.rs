5016a8c7abe48667aa2eb98280bceb894852bfcb
Nick Piaddo <nick.piaddo@protonmail.com>
2024-08-31 14:11:45 +0000

    Create module `core::utils`

--- src/core/utils/mod.rs
+++ src/core/utils/mod.rs
@@ -1,12 +1,10 @@
 // Copyright (c) 2023 Nick Piaddo
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! Shared objects and helper functions.
+//! Utility objects and helper functions.
 
 // From dependency library
 
 // From standard library
 
 // From this library
-pub mod errors;
-pub mod utils;
